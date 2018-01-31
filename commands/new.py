ab3c10a9b0db0d20de85e58daea8c59ca38bcb74
Conn332 <connor.t.day@gmail.com>
2018-01-31 15:46:17 -0500

    changed function of new to set the ledstrip color

--- commands/new.py
+++ commands/new.py
@@ -1,13 +1,20 @@
 import threading
+import pigpio
 class Hello(threading.Thread):
-    def __init__(self):
+    def __init__(self,in_q,out_q):
         super().__init__()
         self.stop = threading.Event()
+        self.pi = pigpio.pi()
+        self.in_q = in_q
+        self.out_q = out_q
     def run(self):
-        count = 0
         while not self.stop.isSet():
-            count += 1
-        print(str(count)+". hello")
+            while not self.in_q.empty():
+                inp = self.in_q.get().split(" ")
+                self.pi.set_PWM_cycle(2,int(inp[0]))
+                self.pi.set_PWM_cycle(3,int(inp[1]))
+                self.pi.set_PWM_cycle(4,int(inp[2]))
+                self.out_q.put("set lights to R:"+inp[0]+" G:"+inp[1]+" B:"+inp[2])
     def join(self, timeout=None):
         self.stop.set()
         super().join(timeout)
